c865be0be99faef15db1348ed79e9d46c1aaefde
suhailii <suhaili.school@gmail.com>
2021-11-15 16:19:35 +0800

    Authenticate login
    authenticate ip address when log in

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, url_for, flash, send_from_directory
+from flask import Flask, render_template, request,session, url_for, flash, send_from_directory
 from werkzeug.utils import redirect
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__) # Create the flask object
+app.secret_key = 'ict2x01' #secretkey need to set for session
 app.config['UPLOAD_FOLDER'] = 'Data'
 
 @app.route('/')
@@ -12,17 +13,41 @@ def default():
 
 @app.route('/index.html', methods=['GET', 'POST'])
 def connect():
-    if request.method == 'POST':
-        return redirect(url_for('dashboard'))
+    session.pop('ip', None)
+
+
     return render_template('index.html')
 
-@app.route('/dashboard.html')
+@app.route('/dashboard.html', methods=['GET', 'POST'])
 def dashboard():
-    return render_template('dashboard.html')
+    if request.method == 'POST':
+        ip = request.form['ip']
+        if ip == "192.168.1.1":
+            #store ip address in session if correct ip
+            session['ip'] = ip
+            return render_template('dashboard.html')
+        else:
+            flash('Invalid IP!')
+            return redirect(url_for('connect'))
+
+    if session.get("ip") is None:
+        flash('Not authenticated!')
+        return redirect(url_for('connect'))
+    elif session['ip'] == "192.168.1.1":
+        return render_template('dashboard.html')            
+    
+
+
 
 @app.route('/challenge1.html')
 def challenge1():
-    return render_template('challenge1.html')
+    #to check for session id for everypage
+    if session.get("ip") is None:
+        flash('Not authenticated!')
+        return redirect(url_for('connect'))
+    elif session['ip'] == "192.168.1.1":
+        return render_template('challenge1.html')
+        
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5000")
