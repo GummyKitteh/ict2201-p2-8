a6a6c3683b22d6dcc04c3137533802c5d70c3be5
JCBlitzNight <2000493@sit.singaporetech.edu.sg>
2021-11-16 13:58:40 +0800

    Improved session management

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ app.config['UPLOAD_FOLDER'] = 'Data'
 
 @app.route('/')
 def default():
-    return render_template('index.html')
+    return render_template('index.html', session=0)
 
 @app.route('/index.html', methods=['GET', 'POST'])
 def connect():
     session.pop('ip', None)
-    return render_template('index.html')
+    return render_template('index.html', session=0)
 
 @app.route('/dashboard.html', methods=['GET', 'POST'])
 def dashboard():
@@ -23,7 +23,7 @@ def dashboard():
         if ip == "192.168.1.1":
             #store ip address in session if correct ip
             session['ip'] = ip
-            return render_template('dashboard.html')
+            return render_template('dashboard.html', session=1)
         else:
             flash('Invalid IP!')
             return redirect(url_for('connect'))
@@ -32,10 +32,7 @@ def dashboard():
         flash('Not authenticated!')
         return redirect(url_for('connect'))
     elif session['ip'] == "192.168.1.1":
-        return render_template('dashboard.html')            
-    
-
-
+        return render_template('dashboard.html', session=1)
 
 @app.route('/challenge1.html')
 def challenge1():
@@ -44,7 +41,7 @@ def challenge1():
         flash('Not authenticated!')
         return redirect(url_for('connect'))
     elif session['ip'] == "192.168.1.1":
-        return render_template('challenge1.html')
+        return render_template('challenge1.html', session=1)
         
 
 @app.route('/challenge2.html')
@@ -54,8 +51,8 @@ def challenge2():
         flash('Not authenticated!')
         return redirect(url_for('connect'))
     elif session['ip'] == "192.168.1.1":
-        return render_template('challenge1.html')
-    return render_template('challenge2.html')
+        return render_template('challenge1.html', session=1)
+    return render_template('challenge2.html', session=1)
 
 @app.route('/challenge3.html')
 def challenge3():
@@ -64,8 +61,8 @@ def challenge3():
         flash('Not authenticated!')
         return redirect(url_for('connect'))
     elif session['ip'] == "192.168.1.1":
-        return render_template('challenge1.html')
-    return render_template('challenge3.html')
+        return render_template('challenge1.html', session=1)
+    return render_template('challenge3.html', session=1)
 
 @app.route('/viewchallenges.html')
 def viewchallenges():
@@ -74,9 +71,9 @@ def viewchallenges():
         flash('Not authenticated!')
         return redirect(url_for('connect'))
     elif session['ip'] == "192.168.1.1":
-        return render_template('challenge1.html')
+        return render_template('challenge1.html', session=1)
         
-    return render_template('viewchallenges.html')
+    return render_template('viewchallenges.html', session=1)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5000")
