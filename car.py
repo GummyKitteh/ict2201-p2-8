6f638f8235718a50718e8ba90c521b87047cc6d0
suhailii <suhaili.school@gmail.com>
2021-12-02 20:45:11 +0800

    Updated whitebox testing to use assert

--- car.py
+++ car.py
@@ -44,7 +44,6 @@ class Car:
         return 
 
 
-
 class CarController:
     def stopCar(carObj):
         carObj.setStatus("stopped")
@@ -53,93 +52,118 @@ class CarController:
 
     def detectObstacle(carObj):
         if carObj.getStatus() == "obstacle":
-            print('OBSTACLE DETECTED!')
             CarController.stopCar(carObj)
             return True
         else:
-            print("NO OBSTACLE")
             return False
 
-        
-        
-
-    def detectEndPoint(self): #not needed?
-        if Car.getStatus == "completed":
+    def detectEndPoint(carObj): 
+        if carObj.getStatus() == "completed":
+            CarController.stopCar(carObj)
             return True
         else:
             return False
 
-    
-
     def executeInstruction(carData):
         carObject = Car(carData.command,0,0,"executing")
+
+        return carObject
+    
+    def sendData(carData):
+        carObject = Car(carData.command,carData.speed,carData.distance,carData.status)
+
+        return carObject
+
+
+   
+def testCase1():
+    print("\nTest Case 1: detectObstacle() and stopCar()\n")
+    #Car object with status of "obstacle"
+    carA = Car("command",10,50,"obstacle")
+    #Car object with status of "executing"
+    carB = Car("command",10,50,"executing")
+    cc = CarController
+
+    try:
+        assert cc.detectObstacle(carA), "Car A: No obstacles detected"
+        print("Car A: Obstacle detected")
+        assert cc.detectObstacle(carB), "Car B: No obstacles detected"
+        print("Car B: Obstacle detected")
+
+    except AssertionError as msg:
+        print(msg)
+
+    try:
+        assert carA.getStatus() == "stopped", "Car A: still moving"
+        print("Car A: has stopped")
+        assert carB.getStatus() == "stopped", "Car B: still moving"
+        print("Car B: has stopped")        
+
+    except AssertionError as msg:
+        print(msg)
+
+def testCase2():
+    print("\nTest Case 2: detectEndPoint() and stopCar() \n")
+    #Car object with status of "completed"
+    carA = Car("command",10,50,"completed")
+    #Car object with status of "executing"
+    carB = Car("command",10,50,"executing")
+    cc = CarController
+
+    try:
+        assert cc.detectEndPoint(carA), "Car A: still executing"
+        print("Car A: completed instruction")
+        assert cc.detectEndPoint(carB), "Car B: still executing"
+        print("Car B: completed instruction")
+
+    except AssertionError as msg:
+        print(msg)
+
+    try:
+        assert carA.getStatus() == "stopped", "Car A: still moving"
+        print("Car A: has stopped")
+        assert carB.getStatus() == "stopped", "Car B: still moving"
+        print("Car B: has stopped")        
+
+    except AssertionError as msg:
+        print(msg)
+
+
+def testCase3():
+    print("\nTest Case 3: executeInstruction() \n")
+    cc = CarController
+    #carData object with the up command.
+    car = carData(command="up", speed=0,distance=0,status="executing")
+    carObject = cc.executeInstruction(car)
+    try:
+        assert carObject.getCommand()=="up", "command not == 'up'"
+        print("Car executed instruction")
         print("Command: "+carObject.getCommand())
         print("Speed: "+str(carObject.getSpeed()))
         print("Distance: "+str(carObject.getDistance()))
         print("Status: "+carObject.getStatus())
 
-            
-        return carObject
-    
-    def sendData(carData):
-        carObject = Car(carData.command,carData.speed,carData.distance,carData.status)
-        print("Command: "+carObject.getCommand())
-        print("Speed: "+ str(carObject.getSpeed()))
-        print("Distance: "+str(carObject.getDistance()))
-        print("Status: "+carObject.getStatus())
-
-
-        return carObject
-
-
-    
-   
-def testCase1():
-    print("\nTest Case 1: detectObstacle() and stopCar()\n")
-
-    testCar = Car("command",10,50,"obstacle")
-    #Car object with status of "obstacle"
-    cc = CarController
-    #print the return of detectObstacle, True if detected, False if not
-    print("Detect object: (T/F)" + str(cc.detectObstacle(testCar)))
-    #print the status of car, (stopped) since detected obstacle
-    print("Car status:"+testCar.getStatus())
-
-def testCase2():
-    print("\nTest Case 2: detectEndPoint() and stopCar() \n")
-    testCar = Car("command",10,50,"completed")
-    #Car object with status of "completed"
-    cc = CarController
-    #print the return of detectEndPoint, True if endpoint, False if not completed
-    print("Detect end point: (T/F)" + str(cc.detectEndPoint(testCar)))
-    #print the status of car, (stopped) since completed 
-    print(testCar.getStatus())
-
-def testCase3():
-    print("\nTest Case 3: executeInstruction() \n")
-    cc = CarController
-    car = carData(command="up", speed=0,distance=0,status="executing")
-    #carData object with the commands.
-    cc.executeInstruction(car)
-    #execute instruction will print details of car object.
+    except AssertionError as msg:
+        print(msg)
     
 
 def testCase4():
-    print("\nTest Case 4: detectObstacle() and stopCar() \n")
+    print("\nTest Case 4: sendData() \n")
     cc = CarController
+    #carData object with the commands
     car = carData(command="left", speed=20,distance=60,status="executing")
-    #carData object with the updated data.
-    cc.sendData(car)
-    #execute instruction will print details of car object.
+    carObject = cc.sendData(car)
+    try:
+        assert (carObject.getCommand()== "left"and carObject.getSpeed()==20 and carObject.getDistance()==60),"Data not sent"
+        print("Car data sent successfully")
+        print("Command: "+carObject.getCommand())
+        print("Speed: "+str(carObject.getSpeed()))
+        print("Distance: "+str(carObject.getDistance()))
+        print("Status: "+carObject.getStatus())
 
+    except AssertionError as msg:
+        print(msg)
 
-
-
-        
-        
-        
-    
-    
     
     
 testCase1()
